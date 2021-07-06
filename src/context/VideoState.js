90801b312ae80d7f275b49db0bd94c4d4b78a08d
Arpan1999 <46858037+Arpan1999@users.noreply.github.com>
2021-07-06 17:03:34 +0530

    Add files via upload

---
+++ src/context/VideoState.js
@@ -0,0 +1,175 @@
+import React, { useState, useEffect, useRef } from "react";
+import VideoContext from "./VideoContex";
+import { io } from "socket.io-client";
+import Peer from "simple-peer";
+
+
+const SERVER_URL = "http://localhost:5000/";
+
+export const socket = io(SERVER_URL);
+
+const VideoState = ({ children }) => {
+    const [callAccepted, setCallAccepted] = useState(false);
+    const [callEnded, setCallEnded] = useState(false);
+    const [stream, setStream] = useState();
+    const [chat, setChat] = useState([]);
+    const [name, setName] = useState("");
+    const [call, setCall] = useState({});
+    const [me, setMe] = useState("");
+    const [userName, setUserName] = useState("");
+    const [otherUser, setOtherUser] = useState("");
+    const [myVdoStatus, setMyVdoStatus] = useState(true);
+    const [userVdoStatus, setUserVdoStatus] = useState();
+    const [msgRcv, setMsgRcv] = useState("");
+  
+    const myVideo = useRef();
+    const userVideo = useRef();
+    const connectionRef = useRef();
+    useEffect(() => {
+        navigator.mediaDevices
+          .getUserMedia({ video: true, audio: true })
+          .then((currentStream) => {
+            setStream(currentStream);
+            myVideo.current.srcObject = currentStream;
+          });
+        if (localStorage.getItem("name")) {
+          setName(localStorage.getItem("name"));
+        }
+        socket.on("me", (id) => setMe(id));
+        socket.on("endCall", () => {
+          window.location.reload();
+        });
+
+        socket.on("updateUserVideo", (currentUserVdoStatus) => {
+            if (currentUserVdoStatus !== null) setUserVdoStatus(currentUserVdoStatus);
+            else setUserVdoStatus(myVdoStatus);
+          });
+      
+          socket.on("callUser", ({ from, name: callerName, signal }) => {
+            setCall({ isReceivingCall: true, from, name: callerName, signal });
+          });
+      
+          socket.on("msgRcv", ({ name, msg: value, sender }) => {
+            setMsgRcv({ value, sender });
+            setTimeout(() => {
+              setMsgRcv({});
+            }, 2000);
+          });
+        }, []);
+
+        const answerCall = () => {
+            setCallAccepted(true);
+            setOtherUser(call.from);
+            const peer = new Peer({ initiator: false, trickle: false, stream });
+        
+            peer.on("signal", (data) => {
+              socket.emit("answerCall", {
+                signal: data,
+                to: call.from,
+                userName: name,
+                myVdoStatus,
+              });
+            });
+        
+            peer.on("stream", (currentStream) => {
+              userVideo.current.srcObject = currentStream;
+            });
+        
+            peer.signal(call.signal);
+        
+            connectionRef.current = peer;
+          };
+        
+          const callUser = (id) => {
+            const peer = new Peer({ initiator: true, trickle: false, stream });
+            setOtherUser(id);
+            peer.on("signal", (data) => {
+              socket.emit("callUser", {
+                userToCall: id,
+                signalData: data,
+                from: me,
+                name,
+              });
+            });
+        
+            peer.on("stream", (currentStream) => {
+              userVideo.current.srcObject = currentStream;
+            });
+
+            socket.on("callAccepted", ({ signal, userName }) => {
+                setCallAccepted(true);
+                setUserName(userName);
+                peer.signal(signal);
+                socket.emit("updateMyVideo", myVdoStatus);
+              });
+          
+              connectionRef.current = peer;
+            };
+          
+            const updateVideo = () => {
+              setMyVdoStatus((currentStatus) => {
+                socket.emit("updateMyVideo", !currentStatus);
+                stream.getVideoTracks()[0].enabled = !currentStatus;
+                return !currentStatus;
+              });
+            };
+          
+            const leaveCall = () => {
+              setCallEnded(true);
+          
+              connectionRef.current.destroy();
+              socket.emit("endCall", { id: otherUser });
+              window.location.reload();
+            };
+          
+            const leaveCall1 = () => {
+              socket.emit("endCall", { id: otherUser });
+            };
+            const sendMsg = (value) => {
+                socket.emit("msgUser", { name, to: otherUser, msg: value, sender: name });
+                let msg = {};
+                msg.msg = value;
+                msg.type = "sent";
+                msg.timestamp = Date.now();
+                msg.sender = name;
+                setChat([...chat, msg]);
+              };
+              return (
+                <VideoContext.Provider
+                  value={{
+                    call,
+                    callAccepted,
+                    myVideo,
+                    userVideo,
+                    stream,
+                    name,
+                    setName,
+                    callEnded,
+                    me,
+                    callUser,
+                    leaveCall,
+                    answerCall,
+                    sendMsg,
+                    msgRcv,
+                    chat,
+                    setChat,
+                    setMsgRcv,
+                    setOtherUser,
+                    leaveCall1,
+                    userName,
+                    myVdoStatus,
+                    setMyVdoStatus,
+                    userVdoStatus,
+                    setUserVdoStatus,
+                    updateVideo,
+                  }}
+                >
+                  {children}
+                </VideoContext.Provider>
+              );
+            };
+            
+            export default VideoState;
+        
+
+  
